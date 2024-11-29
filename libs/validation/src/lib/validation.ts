6e0c277223b73f1fcb04f207f51d34b7232cfd1d
Mohamed Mayallo <mohamed.mayallo1@gmail.com>
2024-11-29 07:59:17 +0200

    some styles

--- libs/validation/src/lib/validation.ts
+++ libs/validation/src/lib/validation.ts
@@ -1,38 +1,39 @@
 import { Validators } from '@angular/forms';
+import { z } from 'zod';
 
 export const loginValidation = {
   email: {
     angular: [Validators.required, Validators.email],
-    nest: [],
+    nest: z.string().email(),
   },
   password: {
     angular: [Validators.required, Validators.minLength(6)],
-    nest: [],
+    nest: z.string().min(6),
   },
 };
 
 export const registerValidation = {
   name: {
     angular: [Validators.required, Validators.minLength(1)],
-    nest: [],
+    nest: z.string().min(1),
   },
   email: {
     angular: [Validators.required, Validators.email],
-    nest: [],
+    nest: z.string().email(),
   },
   password: {
     angular: [Validators.required, Validators.minLength(6)],
-    nest: [],
+    nest: z.string().min(6),
   },
 };
 
 export const createPostValidation = {
   title: {
     angular: [Validators.required, Validators.minLength(1)],
-    nest: [],
+    nest: z.string().min(1),
   },
   content: {
     angular: [Validators.required],
-    nest: [],
+    nest: z.string(),
   },
 };
