5d3739f4c61105178942964533d06298d7625019
Mohamed Mayallo <mohamed.mayallo1@gmail.com>
2024-11-29 15:47:53 +0200

    updated readme

--- apps/backend/src/app/auth/auth.controller.ts
+++ apps/backend/src/app/auth/auth.controller.ts
@@ -1,21 +1,17 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto, User } from '@nest-angular-monorepo/types';
 
 @Controller('auth')
-@ApiTags('Auth')
 export class AuthController {
   @Inject(AuthService) private readonly authService: AuthService;
 
   @Post('/register')
-  @ApiOperation({ summary: 'Register operation' })
   public register(@Body() body: RegisterDto): Promise<User> {
     return this.authService.register(body);
   }
 
   @Post('/login')
-  @ApiOperation({ summary: 'Login operation' })
   public login(@Body() body: LoginDto): Promise<User> {
     return this.authService.login(body);
   }
